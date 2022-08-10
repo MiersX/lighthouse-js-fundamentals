fa394f0b2bcc1d4f46afe9e35a93d3b52ca455d3
MiersX <michael.muirhead123@gmail.com>
2022-08-10 01:25:42 +0000

    Function that determines if a number is odd

---
+++ odd.js
@@ -0,0 +1,12 @@
+const isOdd = function (num) {
+  return num % 2 !== 0
+}
+/*
+console.log(isOdd(11));
+console.log(isOdd(10));
+console.log(isOdd(25));
+console.log(isOdd(46));
+*/
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
